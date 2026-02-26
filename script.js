762d934a08c86cc11c7a410decea3d003a355e6a
jl <102168613+wiqqend@users.noreply.github.com>
2026-02-26 11:25:18 -0500

    removed comments

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-// Reqs
-// Declare a variable (sceneNumber for example) that tracks which scene is currently displayed. Start it at 1.
-// Write a function called advanceScene that:
-// Checks the value of your tracking variable (sceneNumber).
-// Updates the src attribute of the <img> element to the next image. Combine your knowledge of if{} else{} statements with this link to achieve img src manipulation in JS. see link: HTML DOM Image src Property
-// Updates the caption text to match the new image.
-// Increments your tracking variable.
-// When the final image is reached, reveals the hidden end message (hint: use classList.remove to remove a hidden class) and disables the button (hint: use button.disabled = true).
-// Attach an event listener to the button that calls advanceScene on each click.
 
 let sceneNumber = 1;
 advance.addEventListener("click", advanceScene);
