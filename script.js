b62427751257de686bdefdf1fcc780bcdd547114
Jacob <jacobdhartzell@gmail.com>
2026-02-26 13:14:07 -0500

    commit

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function advanceScene() {
 
     if (sceneNumber === 1) {
         document.getElementById("sceneimage").src = "./images/forest1.jpg";
-        document.getElementById("caption").textContent = " You stand at the entrance to the forest. You can see the light filtering through the trees. Hayden tells you that the One Piece is hidden somewhere in this forest.";
+        document.getElementById("caption").textContent = " You stand at the entrance to the forest. You can see the light filtering through the trees. Hayden tells you that nothing is hidden somewhere in this forest.";
         sceneNumber++;
     } else if (sceneNumber === 2) {
         document.getElementById("sceneimage").src = "./images/forest2.jpg";
