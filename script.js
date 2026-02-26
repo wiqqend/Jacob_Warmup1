e0d210b59982a93dbd2bf23715a001ed7595d532
Jacob <jacobdhartzell@gmail.com>
2026-02-26 13:11:21 -0500

    rename

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ function advanceScene() {
 
     if (sceneNumber === 1) {
         document.getElementById("sceneimage").src = "./images/forest1.jpg";
-        document.getElementById("caption").textContent = " You stand at the entrance to the forest. The light is warm. The path ahead is clear. Dr. Ralls is waiting.";
+        document.getElementById("caption").textContent = " You stand at the entrance to the forest. You can see the light filtering through the trees. Hayden tells you that the One Piece is hidden somewhere in this forest.";
         sceneNumber++;
     } else if (sceneNumber === 2) {
         document.getElementById("sceneimage").src = "./images/forest2.jpg";
-        document.getElementById("caption").textContent = " You move deeper in. The trees close around you. The light grows thin. Jamie encourages you to seek the One Piece.";
+        document.getElementById("caption").textContent = " You move deeper in. The trees are thick and the path is narrow. Hayden encourages you to be one with the forest.";
         sceneNumber++;
     } else if (sceneNumber === 3) {
         document.getElementById("sceneimage").src = "./images/forest3.jpg";
-        document.getElementById("caption").textContent = " You have reached the heart of the forest. It is very quiet here. Jiya sees a treasure in the center of the grove.";
+        document.getElementById("caption").textContent = " You have reached the heart of the forest. It is very quiet here. Hayden sees a treasure in the center of the grove.";
         sceneNumber++;
     } else if (sceneNumber === 4) {
 
